cfaab4e926e401f4b1962f7fec1fe5df2b86addd
HanSu Lee <i.hansu.lee@gmail.com>
2023-04-13 13:36:20 +0900

    fix: modify onCreatePage handler in gatsby-node

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,11 +1,10 @@
 import type { GatsbyNode } from 'gatsby';
 
 export const onCreatePage: GatsbyNode['onCreatePage'] = ({ page, actions }) => {
-  const { deletePage, createPage } = actions;
+  const { createPage } = actions;
 
   return new Promise((resolve) => {
     if (page.component === `${__dirname}/src/pages/index/index.tsx`) {
-      deletePage(page);
       createPage({ ...page, path: '/' });
     }
 
